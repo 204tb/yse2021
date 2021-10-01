6ea1485f8af31adb1db0117cd79717d085dd46c6
Sota200099 <200099oy@yse-c.net>
2021-10-01 12:00:25 +0900

    ①～③を編集

--- login.php
+++ login.php
@@ -75,10 +75,10 @@ if (isset($_SESSION['error2'])) {
 		<h1>ログイン</h1>
 		<?php
 		//⑮エラーメッセージの変数に入っている値を表示する
-		echo "<div id='error'>" $errormsg "</div>";
+		echo "<div id='error'>",$errormsg, "</div>";
 		
 		//⑯メッセージの変数に入っている値を表示する
-		echo "<div id='msg'>" $message "</div>";
+		echo "<div id='msg'>", $message, "</div>";
 		?>
 		<form action="login.php" method="post" id="log">
 			<p>
