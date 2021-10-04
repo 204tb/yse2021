d6fd95c1d116065a8d0c905885d3ca668be18a5b
204tb <200204tb@yse-c.net>
2021-10-04 14:24:55 +0900

    ⑤まで着手

--- nyuka.php
+++ nyuka.php
@@ -14,19 +14,21 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①.の処理を行う */) {
+if (session_status()==1) {
 	//②セッションを開始する
+	session_start();
 }
 
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
+if ($_SESSION["login"]){
 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION["error2"]="ログインしてください";
 	//⑤ログイン画面へ遷移する。
+	header("Location:login.php");
 }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
-
 //⑦データベースで使用する文字コードを「UTF8」にする
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
